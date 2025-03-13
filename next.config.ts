18ef51ec6313c4007d027404e39fead2bc688704
Megan Bartlett <megan.bartlett@elexicon.com>
2025-03-13 11:59:29 -0500

    inital Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import path from 'path';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.resolve.modules.push(path.resolve('./src'));
+    return config;
+  },
 };
 
 export default nextConfig;
+
